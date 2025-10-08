1a3f689b364ac8c64d0d2b286fa4b7f586d7eee1
Elias <elopezr2300@alumno.ipn.mx>
2025-10-07 19:19:46 -0600

    cambios

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+import psycopg2
 import os
 
 app = Flask(__name__)
 
+def get_connection():
+    db_url = os.environ.get("DB_URL")
+    return psycopg2.connect(db_url)
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT nombre, calificacion FROM sda_tabla")
+    datos = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template("index.html", datos=datos)
 
 @app.route("/saludo")
 def saludo():
