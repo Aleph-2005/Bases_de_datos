9179ac78b6574492f773585d3d5347030ee0e5c1
Elias <elopezr2300@alumno.ipn.mx>
2025-10-07 21:56:53 -0600

    error

--- app.py
+++ app.py
@@ -25,3 +25,6 @@ def saludo():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    app.run(debug=True)
